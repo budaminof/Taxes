8e0d2e0b28d29e67f8a236febe2f2d9c4dbec5c4
bud aminof <budaminof@gmail.com>
2016-02-16 09:10:23 -0700

    added to fixed and parseInt- but i get error parseInt is not define. have to fixed that

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 module.exports = {
 calcTax: function (money) {
 
-  if (money <= 10) return (money * 0.1);
+  if (money <= 10) return pasreInt((money * 0.1).toFixed(2));
 
-  else if (money > 10 && money <= 20) return ((money -10) * 0.07) + 1 ;
+  else if (money > 10 && money <= 20) return pasreInt((((money -10) * 0.07) + 1).toFixed(2));
 
-  else if (money >20 && money <= 30) return ((money-20) * 0.05) + 1.7;
+  else if (money >20 && money <= 30) return pasreInt((((money-20) * 0.05) + 1.7).toFixed(2));
 
-  else if (money > 30) return ((money-30) * 0.03) + 2.2;
+  else if (money > 30) return pasreInt((((money-30) * 0.03) + 2.2).toFixed(2));
 
-
-}
 
 }
 
-//I was trying to do pasreInt(()).toFixed(2);
-//But for some reason mocha didn't like this.
-//so i dont think this is really complete. 
+}
