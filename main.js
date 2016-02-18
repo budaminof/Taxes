f53af8e891bfcfd4c9d6927b55ad4d517d8fde24
bud aminof <budaminof@gmail.com>
2016-02-18 16:24:38 -0700

    changed parseInt to Number()

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 module.exports = {
 calcTax: function (money) {
 
-  if (money <= 10) return pasreInt((money * 0.1).toFixed(2));
+  if (money <= 10) return Number((money * 0.1).toFixed(2));
 
-  else if (money > 10 && money <= 20) return pasreInt((((money -10) * 0.07) + 1).toFixed(2));
+  else if (money > 10 && money <= 20) return Number((((money -10) * 0.07) + 1).toFixed(2));
 
-  else if (money >20 && money <= 30) return pasreInt((((money-20) * 0.05) + 1.7).toFixed(2));
+  else if (money >20 && money <= 30) return Number((((money-20) * 0.05) + 1.7).toFixed(2));
 
-  else if (money > 30) return pasreInt((((money-30) * 0.03) + 2.2).toFixed(2));
+  else if (money > 30) return Number((((money-30) * 0.03) + 2.2).toFixed(2));
 
 
 }
